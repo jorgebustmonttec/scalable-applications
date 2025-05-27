079458365674af8838779a7b9d793ba4e2cc21c3
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-05-27 21:57:42 +0300

    stuff

--- dab-walking-skeleton/client/astro.config.mjs
+++ dab-walking-skeleton/client/astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import deno from "@deno/astro-adapter";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: deno(),
+});
