1d20302bf582a08f93841ba18663784226537d9d
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-05-27 20:55:12 +0300

    hybrid i think

--- dab-walking-skeleton/server/app.js
+++ dab-walking-skeleton/server/app.js
@@ -80,6 +80,35 @@ const getItems = async () => {
   return items;
 };
 
+// -------------------------- rendering -------------------------
+
+const getInitialItems = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 20));
+  return Array.from({ length: 100 }, (_, i) => ({ id: i, name: `Item ${i}` }));
+};
+
+const getRemainingItems = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 20));
+  return Array.from(
+    { length: 900 },
+    (_, i) => ({ id: i + 100, name: `Item ${i + 100}` }),
+  );
+};
+
+const loadRemaining = async () => {
+  const list = document.getElementById("list");
+  const items = await fetch("http://localhost:8000/items/remaining");
+  const json = await items.json();
+
+  document.getElementById("last")?.remove();
+
+  for (const item of json) {
+    const li = document.createElement("li");
+    li.textContent = item.name;
+    list.appendChild(li);
+  }
+};
+
 // ------------------------- Middleware -------------------------
 app.use("/*", cors());
 app.use("/*", logger());
@@ -181,6 +210,39 @@ app.get("/ssr", async (c) => {
 app.get("/items", async (c) => {
   const items = await getItems();
   return c.json(items);
+});
+
+
+app.get("/items/remaining", async (c) => {
+  const items = await getRemainingItems();
+  return c.json(items);
+});
+
+app.get("/hybrid", async (c) => {
+  const items = await getInitialItems();
+
+  return c.html(`<html>
+    <head>
+      <script>
+        document.addEventListener("DOMContentLoaded", async () => {
+          const list = document.getElementById("list");
+          const items = await fetch("http://localhost:8000/items/remaining");
+          const json = await items.json();
+          for (const item of json) {
+            const li = document.createElement("li");
+            li.textContent = item.name;
+            list.appendChild(li);
+          }
+        });
+      </script>
+    </head>
+    <body>
+      <ul id="list">
+        ${items.map((item) => `<li>${item.name}</li>`).join("")}
+        <li id="last">Loading...</li>
+      </ul>
+    </body>
+  </html>`);
 });
 
 export default app;
